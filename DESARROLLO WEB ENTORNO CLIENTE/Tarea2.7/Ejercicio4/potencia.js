49aacfc8d2a21e88849445847fc90d45d8a255a9
hecto <hectorhcp24@gmail.com>
2023-10-26 12:16:18 +0200

    Añado Ejercicio 3 de la Tarea 2.7

---
+++ DESARROLLO WEB ENTORNO CLIENTE/Tarea2.7/Ejercicio4/potencia.js
@@ -0,0 +1,35 @@
+/**
+ * Crea una página con el botón potencia: invoca una función closure que acumule una potencia. 
+ * En cada invocación eleva a 2 el resultado. La base la tomará de un input NO NUMÉRICO. 
+ * 
+ * @author Héctor Cevallos Paredes
+ * 
+ */
+
+console.log("--Potencia--");
+
+/**
+ * @name potencia
+ * @param {int} base 
+ * @returns int
+ * 
+ * Función clousure que calcula la potencia
+ */
+function potencia(base) {
+    return function (exp) {
+        return exp**base;
+    };
+}
+
+const exp2 = potencia(2);
+
+document.addEventListener("DOMContentLoaded", function () {
+    let sol = 1;
+    const base = document.getElementById("baseInput");
+    const calcularButton = document.getElementById('calcularButton');
+    calcularButton.addEventListener('click', function () { 
+        const exponente = parseInt(base.value);
+        sol *= exp2(exponente);
+        console.log(sol);
+    });
+})
